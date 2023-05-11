53f197abcc74bb21266de62322a6ef764885f414
Ben Krueger <benlkrueger@gmail.com>
2023-05-11 19:59:40 +0200

    New backend approach: Nested Classes and json dump (Probably bad, temporary)

--- src/backend/n-level_sql_hierarchy.py
+++ src/backend/n-level_sql_hierarchy.py
@@ -10,43 +10,50 @@ class NLevelSQLHierarchy:
         '''
 
         self.conn = self._create_connection(db_file)
-        self.cursor = self.conn.cursor
+        self.cursor = self.conn.cursor()
 
         self._initialize_hierarchy(hierarchy_level)
 
     def _initialize_hierarchy(self, hl):
-        if hl == 0:
-            sql_create_parts = """CREATE TABLE IF NOT EXISTS (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL
-            );"""
-        else:
-            sql_create_parts = f"""CREATE TABLE IF NOT EXISTS (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL,
-                parent_id BIGINT,
-                FOREIGN KEY (parent_id)
-                    REFERENCES level{hl} (parent_id)
-            );"""
-        
-            sql_create_sublevels = []
-            for level in range(hl, 1, -1):
-                sql_create_sublevels.append(f"""CREATE TABLE IF NOT EXISTS (
+        '''Initialize the hierarchy and create tables for all of the levels
+
+        :param: hl: the number of groups and subgroups
+        '''
+        with self.conn:
+            if hl == 0:
+                sql_create_parts = """CREATE TABLE IF NOT EXISTS parts (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL
+                );"""
+                self.cursor.execute(sql_create_parts)
+
+            else:
+                sql_create_toplevel = """CREATE TABLE IF NOT EXISTS level1 (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    is_part BOOLEAN DEFAULT FALSE
+                );"""
+                self.cursor.execute(sql_create_toplevel)
+
+                sql_create_parts = f"""CREATE TABLE IF NOT EXISTS parts (
                     id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL,
                     parent_id BIGINT,
                     FOREIGN KEY (parent_id)
-                        REFERENCES level{level} (parent_id)
-                );""")
+                        REFERENCES level{hl} (parent_id)
+                );"""
+                self.cursor.execute(sql_create_parts)
 
-            sql_create_toplevel = """CREATE TABLE IF NOT EXISTS (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL
-            );"""
-
-            with self.conn:
-                self.cursor
-
+                for level in range(hl, 1, -1):
+                    print(f"Making level{level}")
+                    sql_create_level = f"""CREATE TABLE IF NOT EXISTS level{level} (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT NOT NULL,
+                        parent_id BIGINT,
+                        FOREIGN KEY (parent_id)
+                            REFERENCES level{level-1} (parent_id)
+                    );"""
+                    self.cursor.execute(sql_create_level)
 
     def _create_connection(self, db_file):
         try:
@@ -56,3 +63,30 @@ class NLevelSQLHierarchy:
             return e
         
         return conn
+
+    def get_level(self, level):
+        with self.conn:
+            rows = self.cursor.execute(f"SELECT name, is_part FROM level{level}")
+        return rows.fetchall()
+
+    def get_all_parts(self):
+        rows = self.cursor.execute("SELECT name FROM parts")
+        return rows.fetchall()
+
+    def print_tree(self):
+        print("Top-level:")
+        rows = self.cursor.execute("SELECT id, name, is_part FROM level1").fetchall()
+        for row in rows:
+            if row[2] == 1:
+                print(f"- {row[1]} (Part)")
+            elif row[2] == 0:
+                print(f"- {row[1]} (Dir)")
+                self._print_tree_helper(2, row[0])
+
+    def _print_tree_helper(self, level, id, indent=1):
+        
+
+if __name__ == "__main__":
+    h = NLevelSQLHierarchy("database.sqlite", hierarchy_level=2)
+
+    h.print_tree()
---
+++ src/backend/python_json_approach.py
@@ -0,0 +1,98 @@
+import json
+import jsonpickle
+from json import JSONEncoder
+
+class Things:
+    def __init__(self, name, count):
+        self.name = name
+        #self.template = template
+        self.count = count        
+
+    def increase_count(self, delta_count):
+        self.count += delta_count
+
+class Sublevel:
+    def __init__(self, name):
+        self.name = name
+        self.things = {}
+        self.sublevels = {}
+
+    def add_things(self, things):
+        self.things[things.name] = things
+
+    def add_sublevel(self, name):
+        self.sublevels[name] = Sublevel(name)
+
+    def delete_things(self, name):
+        del self.things[name]
+    
+    def delete_sublevel(self, name):
+        del self.sublevels[name]
+
+    def print_tree(self, indent=0):
+        for name, t in self.things.items():
+            print("  " * indent + f"- {name} ({t.__dict__})")
+        for name, sub in self.sublevels.items():
+            print("  " * indent + f"- {name}")
+            sub.print_tree(indent=indent+1)
+
+class Hierarchy(Sublevel):
+    def __init__(self):
+        self.things = {}
+        self.sublevels = {}
+        self.name = "top"
+
+    def add_sublevel_to_path(self, location_path, name):
+        current = self
+        for p in location_path:
+            current = current.sublevels[p]
+
+        current.add_sublevel(name)
+        print(current.sublevels)
+
+    def add_things_to_path(self, location_path, things):
+        current = self
+        for p in location_path:
+            current = current.sublevels[p]
+
+        current.add_things(things)
+
+    def print_tree(self, indent=0):
+        print("Top level: ")
+        super().print_tree()
+
+    def save(self, save_file):
+        hierarchyJSON = jsonpickle.encode(h, unpicklable=True)
+        out = json.dumps(hierarchyJSON, indent=4)
+
+        with open(save_file, 'w') as f:
+            f.write(out)
+
+def load_hierarchy_from_file(file):
+    with open(file, 'r') as f:
+        h = jsonpickle.decode(json.loads(f.read()))
+
+    return h
+
+if __name__ == "__main__":
+    h = Hierarchy()
+    h.add_sublevel("Audio")
+    h.add_sublevel("Video")
+    h.add_sublevel("Licht")
+    h.add_sublevel("Rigging")
+    h.add_sublevel("Allgemein")
+    h.add_sublevel("Sonstiges")
+
+    h.add_things(Things("Stromkabel 30m 16A", 3))
+    h.add_sublevel_to_path(["Video"], "VGA Kabel Box")
+    h.add_things_to_path(["Video", "VGA Kabel Box"], Things("VGA Kabel 3m", 10))
+    h.add_things_to_path(["Video", "VGA Kabel Box"], Things("VGA Kabel 5m", 5))
+    h.add_things_to_path(["Video", "VGA Kabel Box"], Things("VGA Kabel 1m", 13))
+
+    h.print_tree()
+    h.save("backup.json")
+
+    del h
+
+    h = load_hierarchy_from_file("backup.json")
+    h.print_tree()
